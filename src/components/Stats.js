836ef9ec9654a8a7346cfe520cea767e316a69ef
manan2002 <31039184+manan2002@users.noreply.github.com>
2020-06-27 06:35:27 +0000

    Stat component improved. Improved stats component to fix plurality and added Moon icon.

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -3,7 +3,12 @@ import { ItemContext } from "../context/ItemContext";
 
 const Stats = () => {
   const [items] = useContext(ItemContext);
-  return <div className="total">There are {items.length} items.</div>;
+  return (
+    <div className="total">
+      There {items.length === 1 ? "is" : "are"} {items.length}{" "}
+      {items.length === 1 ? "item" : "items"}.
+    </div>
+  );
 };
 
 export default Stats;
--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Sun from "../icons/Sun";
+import Moon from "../icons/Moon";
 const ThemeToggle = () => (
   <button className="theme-btn">
-    Switch
-    {/* <Sun /> */}
+    <Moon />
   </button>
 );
 
